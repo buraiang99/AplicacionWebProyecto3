7376b61d29dc78dc548235e6e87dcfca2112fb9d
buraiang99 <37676815+buraiang99@users.noreply.github.com>
2021-07-08 17:42:29 -0600

    Agregar archivos de proyecto.

---
+++ AplicacionWebProyecto3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AplicacionWebProyecto3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
