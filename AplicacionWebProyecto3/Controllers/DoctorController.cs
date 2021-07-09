acda8608871a1a20694f6324ce7e2a620a77fbf9
buraiang99 <37676815+buraiang99@users.noreply.github.com>
2021-07-08 21:54:51 -0600

    Creacon del metodo registrar Doctor con conexion

--- AplicacionWebProyecto3/Controllers/DoctorController.cs
+++ AplicacionWebProyecto3/Controllers/DoctorController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
@@ -32,6 +33,23 @@ namespace AplicacionWebProyecto3.Controllers
         [HttpPost]
         public IActionResult RegistrarDoctor(DoctorModel doctorModel)
         {
+            if (ModelState.IsValid)
+            {
+                string conexionString = Configuration["ConnectionStrings:DB_Conection"];
+                var connection = new SqlConnection(conexionString);
+
+                string sqlQuery = $"exec sp_insertarDoctor @param_CEDULA = '{doctorModel.Cedula}', " +
+                    $"@param_CODIGO_MEDICO = '{doctorModel.CodigoMedico}', " +
+                    $"@param_PASS = '{doctorModel.Pass}', " +
+                    $"@param_NOMBRE = '{doctorModel.Nombre}', @param_APELLIDOS = '{doctorModel.Apellidos}'";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.CommandType = CommandType.Text;
+                    connection.Open();
+                    command.ExecuteReader();
+                    connection.Close();
+                };
+            }
             return View();
         }
     }
