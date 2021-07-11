0bd8fd7d91a4c3cc4994252e6c86ba6c5ae44e3b
JorgeM81 <madrigal.jorge81@yahoo.es>
2021-07-10 20:16:58 -0600

    view de registrar vacunas, controller para conectarse y guardar en la bd funcional, vacuna model y algunas correcciones del disennno de labels que habia creado en otras view

---
+++ AplicacionWebProyecto3/Controllers/VacunaController.cs
@@ -0,0 +1,48 @@
+﻿using AplicacionWebProyecto3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicacionWebProyecto3.Controllers
+{
+    public class VacunaController : Controller
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public VacunaController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Registrar(VacunaModel vacunaModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string conexionString = Configuration["ConnectionStrings:DB_Connection_Turrialba"];
+                var connection = new SqlConnection(conexionString);
+
+                string sqlQuery = $"exec sp_insertarVacuna @param_CEDULA_PACIENTE = '{vacunaModel.CedulaPaciente}', " +
+                    $"@param_NOMBRE_VACUNA = '{vacunaModel.NombreVacuna}', " +
+                    $"@param_DESCRIPCION = '{vacunaModel.Descripcion}', " +
+                    $"@param_FECHA_APLICACION = '{vacunaModel.FechaAplicacion}', @param_FECHA_PROXIMA = '{vacunaModel.FechaProxima}'";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.CommandType = CommandType.Text;
+                    connection.Open();
+                    command.ExecuteReader();
+                    connection.Close();
+                };
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ AplicacionWebProyecto3/Models/VacunaModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicacionWebProyecto3.Models
+{
+    public class VacunaModel
+    {
+        public int IDVacuna { get; set; }
+        public string CedulaPaciente { get; set; }
+        public string NombreVacuna { get; set; }
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "{0} es requerido")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = false)]
+        [Display(Name = "Fecha de la vacuna")]
+        public string FechaAplicacion { get; set; }
+        [Required(ErrorMessage = "{0} es requerido")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = false)]
+        [Display(Name = "Fecha de la proxima vacuna")]
+        public string FechaProxima { get; set; }
+        public VacunaModel()
+        {
+
+        }//constructor vacio
+
+        public VacunaModel(int iDVacuna, string cedulaPaciente, string nombreVacuna,string descripcion,string fechaAplicacion,string fechaProxima)
+        {
+            IDVacuna = iDVacuna;
+            CedulaPaciente = cedulaPaciente;
+            NombreVacuna = nombreVacuna;
+            Descripcion = descripcion;
+            FechaAplicacion = fechaAplicacion;
+            FechaProxima = fechaProxima;
+        }
+    }//fin class
+}//fin namespace
