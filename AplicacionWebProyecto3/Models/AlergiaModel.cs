b08f0225efec00bbcf58605bb6a10cfa54407dea
JorgeM81 <madrigal.jorge81@yahoo.es>
2021-07-12 19:40:54 -0600

    se agrego el view de alergias y el funcionamiento para agregar, listar, eliminar y actualizar las alergias directamente en la bd

---
+++ AplicacionWebProyecto3/Models/AlergiaModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicacionWebProyecto3.Models
+{
+    public class AlergiaModel
+    {
+        public int IDAlergia { get; set; }
+        public string CedulaPaciente { get; set; }
+        public string NombreAlergia { get; set; }
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "{0} es requerido")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = false)]
+        [Display(Name = "Fecha del diagnostico")]
+        public string FechaDiagnostico { get; set; }
+        public string Medicamentos { get; set; }
+        public AlergiaModel()
+        {
+
+        }//constructor vacio
+
+        public AlergiaModel(int iDAlergia, string cedulaPaciente, string nombreAlergia, string descripcion, string fechaDiagnostico, string medicamentos)
+        {
+            IDAlergia = iDAlergia;
+            CedulaPaciente = cedulaPaciente;
+            NombreAlergia = nombreAlergia;
+            Descripcion = descripcion;
+            FechaDiagnostico = fechaDiagnostico;
+            Medicamentos = medicamentos;
+        }
+    }//fin class
+}//fin namespace
